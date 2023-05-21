48582527fc92b11e1268abcd44839a1909a5b10a
Andrew Morozov <andr.morozov96@gmail.com>
2023-05-21 13:12:31 +0200

    added auth layout

---
+++ client/layouts/auth.tsx
@@ -0,0 +1,32 @@
+import { FC, ReactNode } from 'react';
+import DocumentHead from './_head';
+import styles from '../styles/parts/authLayouts.module.scss';
+
+interface IAuthLayout {
+  title: string;
+  children: ReactNode;
+}
+
+const AuthLayout: FC<IAuthLayout> = ({ children, title }) => {
+  const defaultTitle = 'Auth';
+
+  return (
+    <section className={styles.auth}>
+      <DocumentHead title={title || defaultTitle} />
+
+      <div className={styles.auth__decor}>
+        <img
+          src="/images/auth-bg.png"
+          alt="Auth Background"
+          className={styles.auth__background}
+        />
+      </div>
+
+      <div className={styles.auth__content}>
+        <div className={styles.auth__content_container}>{children}</div>
+      </div>
+    </section>
+  );
+};
+
+export default AuthLayout;
