8a4b58cfca6de36065f1d58e38c0da1c9ce363bf
Andrew Morozov <andr.morozov96@gmail.com>
2023-05-21 21:22:33 +0200

    input testing changes

--- client/UI/components/Input/index.test.tsx
+++ client/UI/components/Input/index.test.tsx
@@ -1,54 +1,41 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import Input from './index';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
+import Input from './index';
+
 describe('Input component', () => {
-  test('renders without errors', () => {
-    render(<Input />);
+  it('renders', () => {
+    const { container } = render(<Input />);
+    expect(container).toBeInTheDocument();
   });
 
-  test('handles input change', () => {
-    const { getByLabelText } = render(<Input label="Test" />);
-    const input = getByLabelText('Test') as HTMLInputElement;
-
-    fireEvent.change(input, { target: { value: 'test value' } });
-
-    expect(input.value).toBe('test value');
-  });
-
-  test('applies "required" style when required prop is true', () => {
-    const { container } = render(<Input label="Test" required />);
+  it('applies "required" style when required prop is true', () => {
+    const { container } = render(<Input required />);
     const label = container.querySelector('label');
-
     expect(label).toHaveClass('required');
   });
 
-  test('applies "disabled" style when disabled prop is true', () => {
-    const { container } = render(<Input label="Test" disabled />);
+  it('applies "disabled" style when disabled prop is true', () => {
+    const { container } = render(<Input disabled />);
     const label = container.querySelector('label');
-
     expect(label).toHaveClass('disabled');
   });
 
-  test('applies "checkbox" style when type prop is "checkbox"', () => {
-    const { container } = render(<Input label="Test" type="checkbox" />);
+  it('applies "checkbox" style when type prop is "checkbox"', () => {
+    const { container } = render(<Input type="checkbox" />);
     const label = container.querySelector('label');
-
     expect(label).toHaveClass('checkbox');
   });
 
-  test('renders label text when label prop is provided', () => {
+  it('renders label text when label prop is provided', () => {
     const { getByText } = render(<Input label="Test" />);
     const labelText = getByText('Test');
-
     expect(labelText).toBeInTheDocument();
   });
 
-  test('does not render label text when label prop is not provided', () => {
+  it('does not render label text when label prop is not provided', () => {
     const { queryByText } = render(<Input />);
     const labelText = queryByText('Test');
-
-    expect(labelText).toBeNull();
+    expect(labelText).not.toBeInTheDocument();
   });
 });
