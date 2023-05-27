56e0ccb2a9b24e420b576e09bfb69d28e78561ef
Andrew Morozov <andr.morozov96@gmail.com>
2023-05-27 19:02:51 +0200

    created AuthenticatedRoute component

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/global.scss';
 import { Provider } from 'react-redux';
 import store from '../services/app-store';
 import type { AppProps } from 'next/app';
+import AuthenticatedRoute from '../services/AuthenticatedRoute';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <AuthenticatedRoute>
+        <Component {...pageProps} />
+      </AuthenticatedRoute>
     </Provider>
   );
 }
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -6,6 +6,34 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <title>FriendZone</title>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&family=Overpass:wght@700&family=Roboto:wght@400;700;900&display=swap"
+          />
+          <link
+            sizes="180x180"
+            rel="apple-touch-icon"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            color="#5bbad5"
+            href="/favicon/safari-pinned-tab.svg"
+          />
+          <meta name="theme-color" content="black" />
         </Head>
         <body>
           <Main />
