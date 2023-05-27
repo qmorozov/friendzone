d6ffb88684f82c0fca614638189e0e70b30dd0fa
Andrew Morozov <andr.morozov96@gmail.com>
2023-05-27 21:03:05 +0200

    change next.config.js and remove pages/index.tsx

--- client/next.config.js
+++ client/next.config.js
@@ -6,4 +6,14 @@ module.exports = nextTranslate({
   optimizeFonts: true,
   optimizeCss: true,
   optimizeSvg: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login',
+        permanent: true,
+      },
+    ];
+  },
 });
