80034dc2ee701164ba8853ba4d51ffed91a1a681
Andrew Morozov <andr.morozov96@gmail.com>
2023-05-28 19:47:50 +0200

    forgot password UI page, additional STEP UI

--- client/modules/auth/pages/ForgotPassword/index.tsx
+++ client/modules/auth/pages/ForgotPassword/index.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+
+import Button from '../../../../UI/components/Button';
+import Input from '../../../../UI/components/Input';
+
+import auth from '../../styles/index.module.scss';
+import login from '../../styles/pages/login.module.scss';
+
 const ForgotPassword = () => {
-  return <div>Forgot Password</div>;
+  return (
+    <div className={login.wrapper}>
+      <h1 className={auth.title}>Reset your password</h1>
+
+      <form>
+        <Input label="Login" />
+        <Input label="E-mail" />
+
+        <Button classes={auth.button}>CONTINUE</Button>
+      </form>
+
+      <div className={auth.auth__footer}>
+        <p>Go back to login screen</p>
+        <Link href="/auth/login">login screen</Link>
+      </div>
+    </div>
+  );
 };
 
 export default ForgotPassword;
--- client/modules/auth/pages/Registration/Steps/additional.tsx
+++ client/modules/auth/pages/Registration/Steps/additional.tsx
@@ -1,4 +1,5 @@
 import Button from '../../../../../UI/components/Button';
+import Input from '../../../../../UI/components/Input';
 
 import auth from '../../../styles/index.module.scss';
 
@@ -8,6 +9,12 @@ const Additional = () => {
       <h1 className={auth.title}>Additional information</h1>
 
       <form>
+        <Input type="textarea" label="Bio" />
+
+        <Input label="Social Media URL #1" />
+        <Input label="Social Media URL #2" />
+        <Input label="Social Media URL #3" />
+
         <Button classes={auth.button}>CONTINUE</Button>
       </form>
     </>
