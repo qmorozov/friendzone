609047d0bfc66c09d2db9e812b415be29caa12d3
aleksandrboichuk <90320430+aleksandrboichuk@users.noreply.github.com>
2023-05-30 21:23:08 +0300

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.setGlobalPrefix('/api/v1');
 
   app.useGlobalPipes(new ValidationPipe());
